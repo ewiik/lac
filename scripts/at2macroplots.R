053c1ac622beeea9d68228103701f067052c9fe7
ewiik <eme.wiik@gmail.com>
2016-01-13 17:47:48 -0600

    got through the updating of these scripts with a few FIXMEs remaining

--- scripts/at2macroplots.R
+++ scripts/at2macroplots.R
@@ -1,4 +1,5 @@
 ## initial plotting and looking at AT2 data
+## only the first depth (0) is a 1cm slice. All others are 2cm slices. Top depth given.
 
 ## load necessary packages
 library("vegan")
@@ -13,12 +14,21 @@ terr <- read.csv("data/private/at2macroallterrcorr.csv")
 plant <- read.csv("data/private/at2macroallplantcorr.csv")
 
 ## read in old strati objects from R scripts in R/
+## these already have taxa lumped e.g. into Saxifragaceae, and unnecessaries *mostly*
+##    removed. FIXME: "rabbit", "black ball", "bubblecushion"
+## list of columns in existing summary plot:
+## Sphagnum pres/abs, cf Nostoc, Oribatid, potobtleaf, simeph, tolyoo, plumrfloat, bconfseed,
+##    hvulgseed, sphaersheath, bryoabun, tubicocoon, rhabdococoon, fredpipt, dapheph,
+##    odigyseed, chameriumseed, sedumseed, poaceaeseed, saxiseed, gnivaseed, juncusseed, 
+##    carexseed, luzulaseed, equispine, mycohyphae, coeno, ledumseed, vaccseed, harrileaf, 
+##    salixseed cassiopeseed, phylloseed, vulilead, sherbleaf, salixbud, enigrseed, callunaseed,
+##    enigrleaf, dintleaf, betulafruit i.e. 41... not leaf abun, rabbit, bubblecushion or blac ball
 opts <- read.csv("data/private/at2macrowaalt.csv") # optima along strati plot
 macrowa <- read.csv("data/private/at2macrowaset.csv") # taxa for strati plots
-macrowa <- macrowa[,-which(names(macrowa) == 'X')]
+macrowa <- macrowa[,-which(names(macrowa) == 'X')] # remove row numbers
 
 ## plot stratiplots
-terrplot <- Stratiplot(terr[,-1], ylab = list(label = "Depth (cm)", cex = 1.2), 
+terrplot <- Stratiplot(terr[,-c(1,3)], ylab = list(label = "Depth (cm)", cex = 1.2), 
                        xlab = list(label = expression("Counts as n(individuals)" ~ 100 ~ cm^{-3} ~ 
                                                         "wet sediment OR abundance score"), cex = 1.2), 
                        terr[,1], type = "h", pages = 1, rev = TRUE, sort = "wa",rev.sort = TRUE, 
@@ -37,27 +47,19 @@ aniplot <- Stratiplot(ani[,-1], ylab = list(label = "Depth (cm)", cex = 1.2),
                       strip = FALSE, topPad =6, lwd = 3, varTypes = "absolute",col = "black", 
                       absoluteSize = 0.5, drawLegend = TRUE)
 
-## FIXME: check if numeric selections correct!
-at2sumtaxa <- cbind(subset(at2macroallterr, select = c(7,8,9,11,12,15,19,22,24,25,27,31)), 
-                    subset(at2macroallani, select = c(3,6,7,9,10,11)), at2macroallplant)
-at2sumplot <- Stratiplot(at2sumtaxa, ylab = list(label = "Depth (cm)", cex = 1.2), 
-                         xlab = list(label = expression("Counts as n(individuals)" ~ 100 ~ cm^{-3} ~ 
-                                                          "wet sediment OR abundance score"), cex = 1.2), 
-                         at2sumtaxa[,1], type = "h", pages = 1, rev = TRUE, sort = "wa",rev.sort = TRUE, 
-                         strip = FALSE, topPad =6, lwd = 3, varTypes = "absolute",col = "black", 
-                         absoluteSize = 0.5, drawLegend = TRUE)
-
-at2terrstratiplotwa <- Stratiplot(at2macrowaset[,c(18:42,45)], at2sumtaxa[,1], type = "h", ylab = "Depth (cm)", 
+## combine plots for terrestrials + aquatics, order by ecological group. Drop juniperinum since there
+##    throughout and not showing anything
+terrstratiplotwa <- Stratiplot(macrowa[,c(18:42,45)], terr[,1], type = "h", ylab = "Depth (cm)", 
                                   xlab = expression("Counts as n(individuals)" ~ 100 ~ cm^{-3} ~ 
                                                       "wet sediment"), pages = 1, rev = TRUE, sort = "var", 
-                                  svar = at2macrowaalt$opt[c(18:42,45)], rev.sort = TRUE, strip = FALSE, 
+                                  svar = opts$opt[c(18:42,45)], rev.sort = TRUE, strip = FALSE, 
                                   topPad= 11, varTypes = "absolute", absoluteSize = 0.5, drawLegend = TRUE, 
                                   col = "black", zones = c(4.5,30.5,62.5,111.5,153.5), 
                                   zoneNames = c(1,2,3,4,"5a","5b"), lwd = 4) # zones from terr cluster analysis
-at2aquastratiplotwa <- Stratiplot(at2macrowaset[,-c(18:45)], at2sumtaxa[,1], type = "h", ylab = "Depth (cm)", 
+aquastratiplotwa <- Stratiplot(macrowa[,-c(16,18:45)], terr[,1], type = "h", ylab = "Depth (cm)", 
                                   xlab = expression("Counts as n(individuals)" ~ 100 ~ cm^{-3} ~ 
                                                       "wet sediment"), pages = 1, rev = TRUE, sort = "var", 
-                                  svar = at2macrowaalt$opt[-c(18:45)], rev.sort = TRUE, strip = FALSE, 
+                                  svar = opts$opt[-c(16,18:45)], rev.sort = TRUE, strip = FALSE, 
                                   topPad= 11, varTypes = "absolute", absoluteSize = 0.5, drawLegend = TRUE, 
                                   col = "black", zones = c(26.5,72.5,107.5,147.5), zoneNames = c(1,2,3,4,5), 
                                   lwd = 4) # zones from terr cluster analysis
@@ -89,53 +91,10 @@ dev.off()
 
 
 
-at2ord <- xyplot(-depth ~ values | ind, data = at2pcastack, type = "o", layout = c(5,1), scales = list(x = list(relation = "free")), col = "black")
+at2ord <- xyplot(-depth ~ values | ind, data = at2pcastack, type = "o", layout = c(5,1), 
+                 scales = list(x = list(relation = "free")), col = "black")
 ## !!! to be made, csv files with the scores for NJA
 setEPS()
 postscript("at2ord.eps", width = 7, height = 5)
 at2ord
 dev.off()
-
-## loi diagram with AT1 and AT2 LOIs for radiocarbon justification...
-gloistack <- at2loi
-gloistack$core <- rep("AT2")
-at1loi <- read.csv("at1loirundepths.csv", header = TRUE)
-at1loi$core <- rep("AT1")
-gloistack <- rbind(gloistack, at1loi)
-at1antloi <- read.csv("at1antloirundepths.csv", header = TRUE)
-gloistack <- rbind(gloistack, at1antloi)
-prettydepths <- seq(0, 280, by = 10)
-gloiplot <- xyplot(-rundepth ~ loi | core, data = gloistack, main = "", type = "o", layout = c(3,1), xlab = "% organic", ylab = "Depth (cm)", scales = list(relation = "same",y = list(at = -prettydepths,labels = prettydepths)), col = "black")
-trellis.focus(name = "panel", column = 2, row = 1)
-panel.text(x = c(8, rep(5, times = 5)), y = c(0,-15,-107, -155, -212, -263), labels = c("Age (ky BP)",".24", "3.6", "5.8", "7.5", "9.5"), cex = 1)
-trellis.unfocus()
-trellis.focus(name = "panel", column = 3, row = 1)
-panel.points(x = c(rep(5, times = 7), 13), y = c(-46,-67.5,-81,-107,-134,-177,-202,-209), pch = 8, col = "black")
-panel.text(x = 10, y = -280, labels = "* = Submitted")
-trellis.unfocus()
-setEPS()
-postscript("greenlanddates.eps")
-#pdf("greenlanddates.pdf", onefile = FALSE)
-gloiplot
-trellis.focus(name = "panel", column = 2, row = 1)
-panel.text(x = c(8, rep(5, times = 5)), y = c(0,-15,-107, -155, -212, -263), labels = c("Age (ky BP)",".24", "3.6", "5.8", "7.5", "9.5"), cex = 1)
-trellis.unfocus()
-trellis.focus(name = "panel", column = 3, row = 1)
-panel.points(x = c(rep(5, times = 7), 13, 2), y = c(-46,-67.5,-81,-107,-134,-177,-202,-209,-280), pch = 8, col = "black")
-panel.text(x = 15, y = -280, labels = " = Submitted")
-trellis.unfocus()
-trellis.focus(name = "panel", column = 1, row = 1)
-panel.points(x = 3, y = -c(35, 75, 130, 170, 220, 280), pch = 3, col = "black")
-panel.text(x = 15, y = -280, labels = " = Requested")
-trellis.unfocus()
-dev.off()
-##prelim dates for at2: 
-prelimdepths <- c(81,134,202,209)
-prelimdates <- c(6.3, 7.8, 8.8, 9.4)
-trellis.focus(name = "panel", column = 3, row = 1)
-panel.text(x = 35, y = -c(prelimdepths), labels = c(prelimdates))
-trellis.unfocus()
-# made a prelim eps
-
-##xyplot(-depth~values|ind, data = isostack, main = "Core data from pilot study", ylab = "depth",xlab = "Isotope values as ppm ",scales = list(relation = "sliced",y = list(at = pretty(-prettydepths),labels = rev(format(pretty(loihaw2$depth))))), type = "o", strip=strip.custom(strip.levels = c(TRUE, TRUE),strip.names=c(FALSE, FALSE), factor.levels= c(expression(paste("carbonate"~delta^{13}~"C")),expression(paste("carbonate"~delta^{18}~"O")),expression(paste("bulk organic"~delta^{13}~"C")),"C/N"), par.strip.text = list(cex = 0.7)), as.table = TRUE,layout = c(4,1))
-expression(paste("total phosphorus, "~mu*gL^{-1}))
--- scripts/at2stats.R
+++ scripts/at2stats.R
@@ -2,7 +2,7 @@
 ##    see multivariatethesis and multivariate2 in R/
 
 ## load necessary packages
-library("mvpart")
+# library("mvpart") not available for R 3.2.3
 library("rioja")
 
 ## read in old strati objects originally from R scripts in R/
@@ -18,32 +18,31 @@ terr <- read.csv("data/private/at2macroallterrcorr.csv")
 plant <- read.csv("data/private/at2macroallplantcorr.csv")
 
 
-## standardise data
-macrowascale <- scale(macrowa, center = FALSE, scale = TRUE) # didn't center, as for Bray, 
-#   I want to keep 0 abundances as 0.
-macrowascale <- as.data.frame(macrowascale)
-at2macrodist <- vegdist(macrowascale, method = "bray", binary = FALSE)
-at2zones <- chclust(at2macrodist)
-plot(at2zones) 
-at2stick <- bstick(at2zones, 6) ## see stratigraphic-prac.pdf
-
-## clustering for aquatics 
-aqua <- cbind(ani[,-c(1,5,6)], plant)
-aquascale <- scale(aqua, center = FALSE, scale = TRUE) ## didn't center, as for Bray,
-##  I want to keep 0 abundances as 0.
+## =================================================================================
+## Stratigraphic zones for AT2 macrofossils: aquatics and terrestrials separately
+## =================================================================================
+## Aquatics: standardise data but no centring since using Bray and want to preserve
+##    0 abundances
+greppedp <- grep('rundepth', names(plant))
+greppeda <- grep('rundepth', names(ani))
+aqua <- cbind(ani[,-greppeda], plant[,-greppedp])
+aquascale <- scale(aqua, center = FALSE, scale = TRUE) 
 aquascale <- as.data.frame(aquascale)
 aquadist <- vegdist(aquascale, method = "bray", binary = FALSE)
 aquazones <- chclust(aquadist)
 plot(aquazones)
-aquastick <- bstick(aquazones, 6) ## see stratigraphic-prac.pdf
-aquastick ## 5 groups for good fit...1-15, 18-38, 39-55, 56-75, 76-104, OR 76-86, 87-104, 
-##  emerge (need to convert to strat depths)
-## FIXME: check if with new rerun, I get real depths not rownumbers. i.e. first column shit
+aquastick <- bstick(aquazones, 8) 
+## see stratigraphic-prac.pdf; I think that the most
+##    parsimonious point is where the red line first goes above the black line going
+##    from left to right on the plot.... maybe...
+aquastick ## 5 groups for good fit?? ...1-15, 18-37, 38-55, 56-75, (76-83, 84-104)
 
-## ================================================================GOT HERE
-## clustering for terrestrials
+## clustering for terrestrials; used terrsub originally, to reduce redundancy of
+##    different parts, same plant, when showing exactly same pattern: ALSO:
+##    In terrsub, some occasionals have been lumped together taxonomically, e.g.
+##    Draba + Cochlearie --> Brassicaceae
+##    Sagina + Minuartia --> Caryophyllaceae
 terrscale <- scale(terrsub, center = FALSE, scale = TRUE) 
-## didn't center, as for Bray, I want to keep 0 abundances as 0.
 terrscale <- as.data.frame(terrscale)
 rowSums(terrscale) ## 91 and 92 yield 0 which is why vegdist no likey
 terrdist <- vegdist(terrscale[-c(92,91),], method = "bray", binary = FALSE)
@@ -53,8 +52,21 @@ terrstick <- bstick(terrzones, 6) ## see stratigraphic-prac.pdf
 terrstick ## even 3 groups for good fit, but taking more will yield ..1-4, 5-17, 18-33, 
 ##  34-57, 58-78, 79-104 (need to convert to strat depths)
 
-at2depths <- list(c(at2macroalltot$rundepth), c(1:104))
-at2depths[[1]][c(1,4,17,33,57,78,104)]
+## let's try clustering for all other terrestrials too.... all aquatics in the start
+##    of macrowa, so will remove aqua cols --> 30 vs 21
+terrsub2 <- macrowa[,-c(1:15)]
+terrscale2 <- scale(terrsub2, center = FALSE, scale = TRUE) 
+terrscale2 <- as.data.frame(terrscale2)
+remove <- which(rowSums(terrscale2) == 0)
+terrdist2 <- vegdist(terrscale2[-remove,], method = "bray", binary = FALSE)
+terrzones2 <- chclust(terrdist2)
+plot(terrzones2)
+terrstick2 <- bstick(terrzones2, 6) ## see stratigraphic-prac.pdf
+terrstick2 ## 4 groups for good fit
+## depends whether or not I believe that redundancy reveals more than just redundancy, e.g.
+##    in form of catchment stability etc. 
+## FIXME: WHY did I not include only 3 or 4 in the summary diagram that I sent to everyone?
+## WHY are there 5 (with a and b)
 
 
 ## NJA wants PCA scores for the data, separately for terrestrial and aquatic.... 
@@ -71,31 +83,16 @@ at2depths[[1]][c(1,4,17,33,57,78,104)]
 ## that leave more numerous remains – i.e. chara and nympheae trichosclerids..  Log (x+1) might be enough  - 
 ##  but I think log10 is a stronger transformation that ln so might be better in this case"
 ## however this from multivariate2: "Gav recommends log transformation for abundance data for PCA"
-at2macroterrsubset <- subset(at2macroallterr, select = c(1,3,4,5,6,8,10,11,12,15,17,18,19,20,21,23,
-                                                         24,26,28,29,30,31,32)) 
-## not Vaccinium in yet, awaiting ID
-at2macroterrsubset[,3] <- at2macroterrsubset[,3] + at2macroterrsubset[,5] 
-## draba E Brassicaceae, so put together with Cochlearia
-names(at2macroterrsubset)[3] <- "Brassicaceae \n (seed)"
-at2macroterrsubset <- at2macroterrsubset[,-5]
-at2macroterrsubset[,11] <- at2macroterrsubset[,11] + at2macroterrsubset[,12]  
-## put sagina and minuartia together into Caryophyllaceae
-names(at2macroterrsubset)[11] <- "Caryophyllaceae \n (seed)"
-at2macroterrsubset <- at2macroterrsubset[,-12]
-write.csv(at2macroterrsubset, "at2macroterrsubset.csv")
-at2macroterrsubset <- read.csv("at2macroterrsubset.csv", row.names = 1)
-terrsubsetnames <- names(at2macroterrsubset)
 
 ## now for pcas
-terrsubsetstand <- scale(at2macroterrsubset, scale = TRUE, center = TRUE) ## should've logged?? 
-##  see above for clustering separately
-terrsubset.pca <- prcomp(terrsubsetstand, tol = sqrt(.Machine$double.eps), scale = FALSE, center = FALSE)
+terrsubstand <- scale(terrsub, scale = TRUE, center = TRUE) ## should've logged?? 
+terrsub.pca <- prcomp(terrsub, tol = sqrt(.Machine$double.eps), scale = FALSE, center = FALSE)
 ## so this could have centered and standardised for me...
-scores(terrsubset.pca)[,c(1:2)] ## PCs 1 and 2
-ordiplot(terrsubset.pca, type = "t", display = "sites")
-points(as.numeric(scores(terrsubset.pca)[,1]), as.numeric(scores(terrsubset.pca)[,2]), pch = 16, 
+scores(terrsub.pca)[,c(1:2)] ## PCs 1 and 2
+ordiplot(terrsub.pca, type = "t", display = "sites")
+points(as.numeric(scores(terrsub.pca)[,1]), as.numeric(scores(terrsub.pca)[,2]), pch = 16, 
        col = rep(c("red","blue","green","black"),times = c(25,25,25,29)), cex = 1) 
-## shows how lovely and complete the horseshoe is....Complete arse!!
+## horseshoe 
 lines(as.numeric(scores(terrsubset.pca)[,1]), as.numeric(scores(terrsubset.pca)[,2])) 
 biplot(terrsubset.pca) ## plots points on according to top and right scales, but alters scores 
 ##  to make plotting nicer, without letting me access those points... aaaghhhh how to plot onto this? 
@@ -103,36 +100,35 @@ biplot(terrsubset.pca) ## plots points on according to top and right scales, but
 terrsubset.pca$sdev
 
 ## and for the logged alternative..:
-terrsubsetstandlog <- scale(log(at2macroterrsubset + 1), scale = TRUE, center = TRUE) 
-terrsubsetlog.pca <- prcomp(terrsubsetstandlog, tol = sqrt(.Machine$double.eps), scale = FALSE, center = FALSE)
-scores(terrsubsetlog.pca)[,c(1:2)] ## PCs 1 and 2
-terrscores <- data.frame(scores(terrsubsetlog.pca)[,c(1:2)])
-terrscores$depth <- at2macroalltot$rundepth
-write.csv(terrscores, "at2_terrestrials_pcascores.csv")
+terrsubstandlog <- scale(log(terrsub + 1), scale = TRUE, center = TRUE) 
+terrsublog.pca <- prcomp(terrsubstandlog, tol = sqrt(.Machine$double.eps), scale = FALSE, center = FALSE)
+scores(terrsublog.pca)[,c(1:2)] ## PCs 1 and 2
+terrscores <- data.frame(scores(terrsublog.pca)[,c(1:2)])
+terrscores$depth <- terr[,1]
 
-ordiplot(terrsubsetlog.pca, type = "t", display = "sites")
-terrsubset.pca$sdev ## vs other. not massively different
-terrsubseteigen <- terrsubset.pca$sdev^2 
-terrsubsetvarexp <- vector()
-for (i in 1:21) {terrsubsetvarexp[i] <- terrsubseteigen[i]/sum(terrsubseteigen) } 
+ordiplot(terrsublog.pca, type = "t", display = "sites")
+terrsub.pca$sdev ## vs other. not massively different
+terrsubeigen <- terrsub.pca$sdev^2 
+terrsubvarexp <- vector()
+for (i in 1:21) {terrsubvarexp[i] <- terrsubeigen[i]/sum(terrsubeigen) } 
 
 ##let's check level of community turnover:
-decorana(log(at2macroterrsubset + 1)) ## All row sums must be >0 in the community matrix: remove empty sites.
-at2terrdca <- decorana(log(at2macroterrsubset[-c(91,92),] + 1))
+## All row sums must be >0 in the community matrix: remove empty sites.
+remove <- which(rowSums(terrsub) == 0)
+at2terrdca <- decorana(log(terrsub[-remove,] + 1))
 #DCA 1 and 2 > 4sd.
-aquastandsubset <- cbind(at2macroallani[,-c(1,5,6)], at2macroallplant)
-decorana(log(aquastandsubset + 1))
+decorana(log(aqua + 1))
 #under 3 here.
 
 ##so let's do ca on terrestrials
-at2terrca <- cca(log(at2macroterrsubset[-c(91,92),] + 1))
+at2terrca <- cca(log(terrsub[-remove,] + 1))
 #Eigenvalues for unconstrained axes:
 #   CA1    CA2    CA3    CA4    CA5    CA6    CA7    CA8 
 #0.5369 0.4380 0.3432 0.3344 0.2265 0.2092 0.1726 0.1557
 plot(at2terrca) # shows how wildly different some are... this may be an artefact of the rare species, 
 ##  as most of my data here are quite low in number i.e. occur in the data set rarely (low abundances, 
 ##  but frequent occurrences, are ok). I could downweight!
-at2terrcadown <- cca(downweight((at2macroterrsubset[-c(91,92),] + 1)))
+at2terrcadown <- cca(downweight((terrsub[-remove,] + 1)))
 plot(at2terrcadown)
 ordiplot(at2terrcadown, type = "t", display = "sites") # looks a lot tighter. and doesn't seem to have an 
 ##  arch effect in that they are not arranged in an arch dictated by time, and it is not hollow...
@@ -141,39 +137,9 @@ scores(at2terrcadown) # but still confused, if e.g. one axis is a distortion of 
 
 
 ## now for aquatics....
-plantstand <- scale(at2macroallplant, scale = TRUE, center = TRUE)
-plant.pca <- prcomp(plantstand, tol = sqrt(.Machine$double.eps), scale = FALSE, center = FALSE) 
-## so this could have centered and standardised for me...
-scores(plant.pca)[,c(1:2)] ## PCs 1 and 2
-ordiplot(plant.pca, type = "t", display = "sites")
-points(as.numeric(scores(plant.pca)[,1]), as.numeric(scores(plant.pca)[,2]), pch = 16, 
-       col = rep(c("red","blue","green","black"),times = c(25,25,25,29)), cex = 1) 
-## shows how lovely and complete the horseshoe is....Complete arse!!
-lines(as.numeric(scores(plant.pca)[,1]), as.numeric(scores(plant.pca)[,2])) 
-biplot(plant.pca) ## plots points on according to top and right scales, but alters scores to make 
-##  plotting nicer, without letting me access those points... aaaghhhh how to plot onto this? 
-##  hence used ordiplot
-
-anistand <- scale(at2macroallani[,-c(1,5,6)], scale = TRUE, center = TRUE) 
-## took out redundant sessoblasts,
-##  and chydorids which show nothing
-ani.pca <- prcomp(anistand, tol = sqrt(.Machine$double.eps), scale = FALSE, center = FALSE) 
-## so this could have centered and standardised for me...
-scores(ani.pca)[,c(1:2)] ## PCs 1 and 2
-ordiplot(ani.pca, type = "t", display = "sites")
-points(as.numeric(scores(ani.pca)[,1]), as.numeric(scores(ani.pca)[,2]), pch = 16, 
-       col = rep(c("red","blue","green","black"),times = c(25,25,25,29)), cex = 1) 
-## top and bottom again close to each other...
-lines(as.numeric(scores(ani.pca)[,1]), as.numeric(scores(ani.pca)[,2])) 
-biplot(ani.pca) ## plots points on according to top and right scales, but alters scores 
-##  to make plotting nicer, without letting me access those points... aaaghhhh how to plot onto this? 
-##  hence used ordiplot
-
-combining ani and plant together:
-  aquastand <- scale(cbind(at2macroallani[,-c(1,5,6)], at2macroallplant), scale = TRUE, center = TRUE) 
+aquastand <- scale(aqua, scale = TRUE, center = TRUE) 
 ## took out redundant sessoblasts, and chydorids which show nothing
 aqua.pca <- prcomp(aquastand, tol = sqrt(.Machine$double.eps), scale = FALSE, center = FALSE) 
-## so this could have centered and standardised for me...
 scores(aqua.pca)[,c(1:2)] ## PCs 1 and 2
 ordiplot(aqua.pca, type = "t", display = "sites")
 points(as.numeric(scores(aqua.pca)[,1]), as.numeric(scores(aqua.pca)[,2]), pch = 16, 
@@ -185,14 +151,11 @@ biplot(aqua.pca) ## plots points on according to top and right scales, but alter
 ##  hence used ordiplot
 
 #and logged alternative:
-aqualogstand <- scale(log(cbind(at2macroallani[,-c(1,5,6)], at2macroallplant) +1), scale = TRUE, center = TRUE) 
-## took out redundant sessoblasts, and chydorids which show nothing
+aqualogstand <- scale(log(aqua + 1), scale = TRUE, center = TRUE) 
 aqualog.pca <- prcomp(aqualogstand, tol = sqrt(.Machine$double.eps), scale = FALSE, center = FALSE) 
-## so this could have centered and standardised for me...
 scores(aqualog.pca)[,c(1:2)] ## PCs 1 and 2
 aquascores <- data.frame(scores(aqualog.pca)[,c(1:2)])
-aquascores$depth <- at2macroalltot$rundepth
-write.csv(aquascores, "at2_aquatics_pcascores.csv")
+aquascores$depth <- terr[,1]
 ordiplot(aqualog.pca, type = "t", display = "sites")
 points(as.numeric(scores(aqualog.pca)[,1]), as.numeric(scores(aqualog.pca)[,2]), pch = 16, 
        col = rep(c("red","blue","green","black"),times = c(25,25,25,29)), cex = 1) 
@@ -203,15 +166,3 @@ biplot(aqualog.pca) ## plots points on according to top and right scales, but al
 ##  hence used ordiplot
 # there is still a horseshoe effect so not too confident about this, I think CA would be better!
 
-## LOI data to be included:
-at2loi <- read.csv("at2loirundepths.csv", header = TRUE)
-at2pcastack <- stack(as.data.frame(scores(at2terrcadown)[[2]]))
-at2pcastack$depth <- rep(as.numeric(at2macroalltot$rundepth[-c(91,92)]))
-at2pcastack$ind <- as.character(at2pcastack$ind)
-at2pcastack$ind[at2pcastack$ind == "CA1"] <- "terrCA1"
-at2pcastack$ind[at2pcastack$ind == "CA2"] <- "terrCA2"
-at2pcastack <- rbind(at2pcastack, data.frame(values = at2loi$loi[1:166], ind = rep("loi"), 
-                                             depth = at2loi$rundepth[1:166]))
-at2pcastackaqua <- stack(data.frame(aquaPC1 = scores(aqualog.pca)[,1], aquaPC2 = scores(aqualog.pca)[,2]))
-at2pcastackaqua$depth <- rep(as.numeric(at2macroalltot$rundepth))
-at2pcastack <- rbind(at2pcastack, at2pcastackaqua)
